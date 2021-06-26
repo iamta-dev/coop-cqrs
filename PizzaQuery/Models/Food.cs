4852ca0f3d18e46516e34e7c73de5f6d698650a8
paicrush <dcdc07411@gmail.com>
2021-06-26 22:47:46 +0700

    clear

--- PizzaQuery/Models/Food.cs
+++
@@ -1,9 +0,0 @@
-namespace ContosoFood.Models
-{
-    public class Food
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-    }
-}
