09a006b39a6f766c6db81a8272f1a434c1978a17
paicrush <dcdc07411@gmail.com>
2021-06-26 22:51:47 +0700

    PizzaQuery

---
+++ PizzaQuery/Controllers/PizzaController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Pizzaquery.Models;
+using Pizzaquery.Services;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+    PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
