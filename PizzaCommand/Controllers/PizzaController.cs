54ddd31b4e3767a12830e8bd9063e7b7efc7e051
pond2013 <pond.pawaraid@kkumail.com>
2021-06-26 21:13:42 +0700

    ADD PizzaCommand

---
+++ PizzaCommand/Controllers/PizzaController.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+
+        // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
+
+        // PUT action
+        // [HttpPut("{id}")]
+        // public IActionResult Update(int id, Pizza pizza)
+        // {
+        //     if (id != pizza.Id)
+        //         return BadRequest();
+
+        //     var existingPizza = PizzaService.Get(id);
+        //     if (existingPizza is null)
+        //         return NotFound();
+
+        //     PizzaService.Update(pizza);
+
+        //     return NoContent();
+        // }
+
+        // DELETE action
+        // [HttpDelete("{id}")]
+        // public IActionResult Delete(int id)
+        // {
+        //     var pizza = PizzaService.Get(id);
+
+        //     if (pizza is null)
+        //         return NotFound();
+
+        //     PizzaService.Delete(id);
+
+        //     return NoContent();
+        // }
+    }
+}
