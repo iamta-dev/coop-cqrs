a70069dfbaf978feb7decc63addea0c3004cb114
PHUICMT <icmtchannel@gmail.com>
2021-06-27 19:54:11 +0700

    change PizzaCommand launchSettings.json

--- PizzaQuery/Models/Pizza.cs
+++
@@ -1,9 +0,0 @@
-namespace Pizzaquery.Models
-{
-    public class Pizza
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public bool IsGlutenFree { get; set; }
-    }
-}
